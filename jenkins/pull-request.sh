289a9f85c4c35aaa3c08ee663272f9adbedb8257
YU88John <mtk.john88@gmail.com>
2023-12-14 20:19:54 +0400

    Add stage - Create pull request

---
+++ jenkins/pull-request.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+repositoryOwner="YU88John"  
+repositoryName="k8s-gitops-manifests-repo"  
+baseBranch="main"  
+headBranch="dev"  
+pullRequestTitle="Updated image version - Build #${BUILD_NUMBER}"
+pullRequestBody="Please pull these awesome changes in!"
+
+apiUrl="https://api.github.com/repos/${repositoryOwner}/${repositoryName}/pulls"
+payload="{
+    \"title\": \"${pullRequestTitle}\",
+    \"body\": \"${pullRequestBody}\",
+    \"head\": \"${repositoryOwner}:${headBranch}\",
+    \"base\": \"${baseBranch}\"
+}"
+
+curl -L \
+    -X POST \
+    -H 'Accept: application/vnd.github+json' \
+    -H 'Authorization: Bearer '${GITHUB_TOKEN} \
+    -H 'X-GitHub-Api-Version: 2022-11-28' \
+    ${apiUrl} \
+    -d "${payload}"
